50851ee170ff81884df4cb08b4a7b29ac5d91883
Roar Thronæs <roart@nvg.ntnu.no>
2019-09-01 12:56:48 +0200

    More neural net continual learning (I100).

---
+++ mydatasets.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.optim import *
+import torchvision
+import matplotlib.pyplot as plt
+
+def getdataset(myobj):
+    if myobj.dataset == 'mnist':
+        return getmnist()
+    if myobj.dataset == 'dailymintemperatures':
+        return getdailymintemperatures(myobj)
+
+def getmnist():
+    dl = DataLoader(torchvision.datasets.MNIST('/tmp/datasets/mnist', train=True, download=True))
+
+    tensor = dl.dataset.data
+    tensor = tensor.to(dtype=torch.float32)
+    tr = tensor.reshape(tensor.size(0), -1) 
+    tr = tr/128
+    targets = dl.dataset.targets
+    targets = targets.to(dtype=torch.long)
+
+    x_train = tr[0:50000]
+    y_train = targets[0:50000]
+    x_valid = tr[50000:60000]
+    y_valid = targets[50000:60000]
+
+    y_valid = y_valid.to(dtype=torch.long)
+    
+    #bs=64
+
+    #train_ds = TensorDataset(x_train, y_train)
+    #train_dl = DataLoader(train_ds, batch_size=bs, drop_last=False, shuffle=True)
+
+    #valid_ds = TensorDataset(x_valid, y_valid)
+    #valid_dl = DataLoader(valid_ds, batch_size=bs * 2)
+
+    #loaders={}
+    #loaders['train'] = train_dl
+    #loaders['valid'] = valid_dl
+    print(type(x_train), x_train.shape, x_train.type())
+    print(type(y_train), y_train.shape, y_train.type())
+    return x_train, y_train, x_valid, y_valid, 784, 10
